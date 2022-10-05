f858c296afd8792d2f38888d775a5ac8ad9655dd
salnuraqidah <salnuraqidah@gmail.com>
2022-10-05 14:25:04 +0700

    save to excel

--- project_steampowered_com/main.py
+++ project_steampowered_com/main.py
@@ -2,6 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 import json
 import pandas as pd
+import os
+
 
 url = 'https://store.steampowered.com/search/?term=gta'
 
@@ -13,6 +15,10 @@ def get_data(url):
 def parse(data):
     result = []
     soup = BeautifulSoup(data, 'html.parser')
+    try:
+      os.mkdir('json_result')
+    except FileExistsError:
+        pass
     contents = soup.find('div',attrs={'id':'search_resultsRows'})
     games = contents.find_all('a')
     
@@ -45,8 +51,15 @@ def output (datas: list):
     for i in datas:
         print(i)
 
+def generate_data(result, filename):
+    df = pd.DataFrame(result)
+    df.to_excel(f'{filename}.xlsx',index=False)
+
+
 if __name__ == '__main__':
     data = get_data(url)
 
     final_data = parse(data)
+    namafile = input('Masukkan nama file:')
+    generate_data(final_data,namafile)
     output(final_data)
